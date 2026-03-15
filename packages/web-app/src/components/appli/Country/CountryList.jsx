5558c7f2f9bcf6cfeab92c2c600ae03c905fc571
Paul AUBERTIN <paul.aubertin.fr@gmail.com>
2026-03-16 00:31:31 +0100

    fix contry list layout

--- packages/web-app/src/components/appli/Country/CountryList.jsx
+++ packages/web-app/src/components/appli/Country/CountryList.jsx
@@ -10,7 +10,7 @@ import {
   Paper
 } from '@mui/material';
 import PropTypes from 'prop-types';
-import ScrollableContent from '../../common/Layouts/Fixed/ScrollableContent';
+import FixedContent from '../../common/Layouts/Fixed/FixedContent';
 import GCLink from '../../common/GCLink';
 import getLocalizedCountryName from '../../../helpers/countryName';
 import { AVAILABLE_LANGUAGES } from '../../../conf/config';
@@ -23,7 +23,7 @@ const CountryList = ({ countries = [] }) => {
   };
 
   return (
-    <ScrollableContent
+    <FixedContent
       title={formatMessage({
         id: 'Countries : Sovereign countries and autonomous territories (ISO 3166-1)'
       })}
