3347597a3e8bc849571b2bba805a092478dc8d85
Paul AUBERTIN <paul.aubertin.fr@gmail.com>
2026-07-20 12:04:45 +0200

    wip

--- packages/web-app/src/pages/homepage/index.jsx
+++ packages/web-app/src/pages/homepage/index.jsx
@@ -18,6 +18,7 @@ const RecentChanges = lazy(() => import('./RecentChanges'));
 const LatestBlogNewsSection = lazy(() => import('./LatestBlogNewsSection'));
 const Association = lazy(() => import('./Association'));
 const PartnersSection = lazy(() => import('./PartnersSection'));
+const TopoViewer = lazy(() => import('./TopoViewer'));
 
 const HomepageWrapper = styled('div')(({ theme }) => ({
   fontFamily: theme.typography.fontFamily
@@ -45,6 +46,7 @@ const HomePage = () => {
         <HeroStats />
         <Welcome />
         <Suspense fallback={<Box sx={{ minHeight: 200 }} />}>
+          <TopoViewer />
           <RandomEntry />
           <RecentChanges />
           <LatestBlogNewsSection />
